301c6e402d091ba796300f6eb296dda347a1a947
Ihor Kliushnyk <igorator746@email.com>
2025-11-27 11:56:53 +0200

    Refactor code structure for improved readability and maintainability

--- client/src/redux/services/userApi.ts
+++ client/src/redux/services/userApi.ts
@@ -2,17 +2,20 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const userApi = createApi({
   reducerPath: 'userApi',
-  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+    credentials: 'include',
+  }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (credentials) => ({
         url: '/auth/login',
         method: 'POST',
         body: credentials,
-        credentials: 'include',
       }),
     }),
   }),
 });
 
 export const { useLoginMutation } = userApi;
+
